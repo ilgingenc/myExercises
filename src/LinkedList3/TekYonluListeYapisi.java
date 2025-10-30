b84477000072ef9d108cae6dd6c27f87f19d111e
user <ilginngnc@gmail.com>
2025-10-31 00:44:10 +0300

    Linked List örnekleri

--- src/LinkedList3/TekYonluListeYapisi.java
+++ src/LinkedList3/TekYonluListeYapisi.java
@@ -1,10 +1,22 @@
 package LinkedList3;
 
 public class TekYonluListeYapisi {
-    Node head=null;
-    Node tail=null;
+    Node head = null;
+    Node tail = null;
 
-    void basaEkle(int x){
+    void basaEkle(int x) {
+        Node eleman = new Node();
+        eleman.data = x;
 
+        if (head == null) {
+            eleman.next = null;
+            head = eleman;
+            tail = eleman;
+            System.out.println("listeye ilk eleman eklendi.");
+        } else {
+            eleman.next = head;
+            head = eleman;
+            System.out.println("başa eleman eklendi.");
+        }
     }
 }
--- src/LinkedList3/Uygulama.java
+++ src/LinkedList3/Uygulama.java
@@ -2,6 +2,13 @@ package LinkedList3;
 
 public class Uygulama {
     public static void main(String[] args) {
+        TekYonluListeYapisi list = new TekYonluListeYapisi();
+        list.basaEkle(10);
+        list.basaEkle(20);
+        list.basaEkle(30);
+        list.basaEkle(40);
+
+        System.out.println(list.head.data);
 
     }
 }
