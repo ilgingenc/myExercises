fb3ab95a22cdbd0d1f80a18cca5176c4e869b023
user <ilginngnc@gmail.com>
2025-11-03 02:04:57 +0300

    Linked List örnekleri

--- src/LinkedList4/TekYönlüListeYapisi.java
+++ src/LinkedList4/TekYönlüListeYapisi.java
@@ -19,20 +19,54 @@ public class TekYönlüListeYapisi {
             System.out.println("listeye eleman eklendi.");
         }
     }
-    void sonekle(int x){
-        Node eleman= new Node();
-        eleman.data= x;
 
-        if (head==null){
+    void sonekle(int x) {
+        Node eleman = new Node();
+        eleman.data = x;
+
+        if (head == null) {
+            eleman.next = null;
+            head = eleman;
+            tail = eleman;
+            System.out.println("listeye ilk eleman eklendi.");
+        } else {
+            eleman.next = null;
+            tail.next = eleman;
+            tail = eleman;
+        }
+
+    }
+    void arayaEkle(int indis ,int x){
+        Node eleman = new Node();
+        eleman.data=x;
+        if (head==null && indis==0){
             eleman.next=null;
             head=eleman;
             tail=eleman;
             System.out.println("listeye ilk eleman eklendi.");
         }
+        else if (head!=null && indis==0){
+            eleman.next=head;
+            head=eleman;
+            System.out.println(indis+ "indisinci sıraya yeni eleman eklendi.");
+        }
         else {
-            eleman.next=null;
-            tail.next=eleman;
-            tail=eleman;
+
+        }
+    }
+
+    void yazdir() {
+        if (head == null){
+            System.out.println("liste yapısı boş.");
+        }
+        else{
+            Node temp = head;
+            System.out.print("baş->");
+            while(temp != null){
+                System.out.print(temp.data + " -> ");
+                temp=temp.next;
+            }
+            System.out.print("son");
         }
     }
 }
--- src/LinkedList4/Uygulama.java
+++ src/LinkedList4/Uygulama.java
@@ -2,6 +2,11 @@ package LinkedList4;
 
 public class Uygulama {
     public static void main(String[] args) {
+    TekYönlüListeYapisi list= new TekYönlüListeYapisi();
+    list.basaekle(5);
+    list.basaekle(6);
+    list.basaekle(7);
 
+    list.yazdir();;
     }
 }
