53bebe6395706fa84cc7633778b9bf3884a676be
user <ilginngnc@gmail.com>
2025-11-04 00:28:22 +0300

    Linked List örnekleri

--- src/LinkedList4/TekYönlüListeYapisi.java
+++ src/LinkedList4/TekYönlüListeYapisi.java
@@ -36,35 +36,17 @@ public class TekYönlüListeYapisi {
         }
 
     }
-    void arayaEkle(int indis ,int x){
-        Node eleman = new Node();
-        eleman.data=x;
-        if (head==null && indis==0){
-            eleman.next=null;
-            head=eleman;
-            tail=eleman;
-            System.out.println("listeye ilk eleman eklendi.");
-        }
-        else if (head!=null && indis==0){
-            eleman.next=head;
-            head=eleman;
-            System.out.println(indis+ "indisinci sıraya yeni eleman eklendi.");
-        }
-        else {
 
-        }
-    }
 
     void yazdir() {
-        if (head == null){
+        if (head == null) {
             System.out.println("liste yapısı boş.");
-        }
-        else{
+        } else {
             Node temp = head;
             System.out.print("baş->");
-            while(temp != null){
+            while (temp != null) {
                 System.out.print(temp.data + " -> ");
-                temp=temp.next;
+                temp = temp.next;
             }
             System.out.print("son");
         }
